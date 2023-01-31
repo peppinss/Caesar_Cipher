09dc313e0469705c90acb3bf3b14cdc3bf593cff
peppinss <peppeke123@gmail.com>
2023-01-31 01:53:31 +0000

    v1.3

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-#Write your code below this line 👇
-import math
-def paint_calc(height, width, cover):
-    test = math.ceil((height * width) / cover)
-    print(f"You'll need {test} cans of paint.")
+# Write your code below this line 👇
+def prime_checker(number):
+    y = 0
+    if str(number) in primenumber:
+        print("It's a prime number.")
+    else:
+        for x in range(2,10):
+            if number % x != 0:
+                y += 1
+                print(y)
+            else:
+                print(y)
+                continue
+        if y == 8:
+            print("It's a prime number.")
+        else:
+            print("It's not a prime number.")
 
+primenumber = "1", "2", "3", "5", "7"
+# Write your code above this line 👆
 
-
-
-
-
-#Write your code above this line 👆
-# Define a function called paint_calc() so that the code below works.
-
-# 🚨 Don't change the code below 👇
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
-
+# Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
